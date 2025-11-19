045c52fd169f4e31849cea74e796076e67a8aca3
Yusuf Bakay <110858019+yb50@users.noreply.github.com>
2025-11-19 23:05:03 +0900

    feat: add 'Your First Component' tutorial code
    - Implemented and completed challenge exercises from React docs' 'Your First Component' tutorial.

---
+++ learn-react/src/(2)-your-first-component/Challenges.jsx
@@ -0,0 +1,92 @@
+/* --- Challenge 1 --- */
+
+// function Profile() {
+//   return (
+//     <img
+//       src="https://i.imgur.com/lICfvbD.jpg"
+//       alt="Aklilu Lemma"
+//     />
+//   );
+// }
+
+// Solution
+
+export default function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/lICfvbD.jpg"
+      alt="Aklilu Lemma"
+    />
+  );
+}
+
+/* --- Challenge 2 --- */
+
+// export default function Profile() {
+//   return
+//   <img src="https://i.imgur.com/jA8hHMpm.jpg" alt="Katsuko Saruhashi" />;
+// }
+
+// Solution
+
+export default function Profile() {
+  return <img src="https://i.imgur.com/jA8hHMpm.jpg" alt="Katsuko Saruhashi" />;
+}
+
+/* --- Challenge 3 --- */
+
+// function profile() {
+//   return (
+//     <img
+//       src="https://i.imgur.com/QIrZWGIs.jpg"
+//       alt="Alan L. Hart"
+//     />
+//   );
+// }
+
+// export default function Gallery() {
+//   return (
+//     <section>
+//       <h1>Amazing scientists</h1>
+//       <profile />
+//       <profile />
+//       <profile />
+//     </section>
+//   );
+// }
+
+// Solution
+
+function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/QIrZWGIs.jpg"
+      alt="Alan L. Hart"
+    />
+  );
+}
+
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
+
+/* --- Challenge 4 --- */
+
+// Write your component below!
+// ...
+// ...
+
+// Solution
+
+export default function Congratulations() {
+  return (
+    <h1>Good job!</h1>
+  );
+}
---
+++ learn-react/src/(2)-your-first-component/Profile2.jsx
@@ -0,0 +1,18 @@
+function Profile2() {
+  return (
+    <div>
+      <img src="https://i.imgur.com/MK3eW3Am.jpg" alt="Katherine Johnson"/>
+    </div>
+  );
+}
+
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile2 />
+      <Profile2 />
+      <Profile2 />
+    </section>
+  );
+}
