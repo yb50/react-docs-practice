3f16228c3702a9b9235cc9a917b115801424708a
Yusuf Bakay <110858019+yb50@users.noreply.github.com>
2025-12-02 18:09:26 +0900

    feat: 'Rendering Lists' lesson - first challenge
    - Implemented the first challenge: used map() and filter() to render a list of items from an array, added `key` props for each list item.

---
+++ learn-react/src/(8)-rendering-lists/(3)-Challenges/(1)-Challenge-1/App.jsx
@@ -0,0 +1,77 @@
+/* --- Challenge 1 of 4 --- */
+
+import { getImageUrl } from "./utils";
+import { people } from "./data";
+
+/*
+import { people } from './data.js';
+import { getImageUrl } from './utils.js';
+
+export default function List() {
+  const listItems = people.map(person =>
+    <li key={person.id}>
+      <img
+        src={getImageUrl(person)}
+        alt={person.name}
+      />
+      <p>
+        <b>{person.name}:</b>
+        {' ' + person.profession + ' '}
+        known for {person.accomplishment}
+      </p>
+    </li>
+  );
+  return (
+    <article>
+      <h1>Scientists</h1>
+      <ul>{listItems}</ul>
+    </article>
+  );
+}
+*/
+
+// Solution
+
+export default function List() {
+  const chemists = people.filter(person => person.profession === 'chemist');
+
+  const everyoneElse = people.filter(person => person.profession !== 'chemist');
+
+  return (
+    <article>
+      <h1>Scientists</h1>
+      <h2>Chemists</h2>
+      <ul>
+        {chemists.map(person =>
+          <li key={person.id}>
+            <img 
+              src={getImageUrl(person)}
+              alt={person.name}
+            />
+            <p>
+              <b>{person.name}:</b>
+              {' ' + person.profession + ' '}
+              known for {person.accomplishment}
+            </p>
+          </li>
+        )}
+      </ul>
+      <h2>Everyone Else</h2>
+      <ul>
+        {everyoneElse.map(person =>
+          <li key={person.id}>
+            <img 
+              src={getImageUrl(person)}
+              alt={person.name}
+            />
+            <p>
+              <b>{person.name}</b>
+              {' ' + person.profession + ' '}
+              known for {person.accomplishment}
+            </p>
+          </li>
+        )}
+      </ul>
+    </article>
+  );
+}
