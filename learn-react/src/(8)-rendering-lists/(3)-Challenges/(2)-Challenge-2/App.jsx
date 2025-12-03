20987d5e42c387a2a58fb85de0a00883b98fc480
Yusuf Bakay <110858019+yb50@users.noreply.github.com>
2025-12-03 21:19:18 +0900

    feat: 'Rendering Lists' lesson - second challenge
    - Implemented nested list rendering for recipes and ingredients.
    - Added keys for recipe and ingredient items to satisfy React’s list requirements.
    - Updated RecipeList component to map recipes -> ingredients with proper JSX structure.

---
+++ learn-react/src/(8)-rendering-lists/(3)-Challenges/(2)-Challenge-2/App.jsx
@@ -0,0 +1,35 @@
+/* --- Challenge 2 of 4 --- */
+
+/*
+import { recipes } from './data.js';
+
+export default function RecipeList() {
+  return (
+    <div>
+      <h1>Recipes</h1>
+    </div>
+  );
+}
+*/
+
+// Solution
+
+export default function RecipeList() {
+  return (
+    <div>
+      <h1>Recipes</h1>
+      {recipes.map(recipe =>
+        <div key={recipe.id}>
+          <h2>{recipe.name}</h2>
+          <ul>
+            {recipe.ingredients.map(ingredient =>
+              <li key={ingredient}>
+                {ingredient}
+              </li>
+            )}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
