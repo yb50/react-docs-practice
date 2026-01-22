94fc61c83f4de63251ae4782132eb629158faead
Yusuf Bakay <110858019+yb50@users.noreply.github.com>
2026-01-22 22:19:14 +0900

    feat: 'keeping ... pure' lesson: second challenge
    - Remove shared currentPerson state and make Profile, Header, and Avatar pure by passing person data through props.

---
+++ learn-react/src/(9)-keeping-components-pure/(1)-challenge/(2)-challenge-2/App.jsx
@@ -0,0 +1,62 @@
+/*
+import Panel from './Panel.js';
+import { getImageUrl } from './utils.js';
+
+let currentPerson;
+
+export default function Profile({ person }) {
+  currentPerson = person;
+  return (
+    <Panel>
+      <Header />
+      <Avatar />
+    </Panel>
+  )
+}
+
+function Header() {
+  return <h1>{currentPerson.name}</h1>;
+}
+
+function Avatar() {
+  return (
+    <img
+      className="avatar"
+      src={getImageUrl(currentPerson)}
+      alt={currentPerson.name}
+      width={50}
+      height={50}
+    />
+  );
+}
+*/
+
+// Solution
+
+import Panel from './Panel.js';
+import { getImageUrl } from './utils.js';
+
+export default function Profile({ person }) {
+  return (
+    <Panel>
+      <Header person={person} />
+      <Avatar person={person} />
+    </Panel>
+  )
+}
+
+function Header({ person }) {
+  return <h1>{person.name}</h1>;
+}
+
+function Avatar({ person }) {
+  return (
+    <img
+      className="avatar"
+      src={getImageUrl(person)}
+      alt={person.name}
+      width={50}
+      height={50}
+    />
+  );
+}
